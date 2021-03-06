1cec4e8e7cb245fa21f446041c76e2bf40435e5e
Kaleb Mesfin <kalemesfin12@gmail.com>
2021-03-06 11:04:11 +0300

    findBottomlefttree

---
+++ Week 5/findBottomLeftTreeValue.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def findBottomLeftValue(self, root: TreeNode) -> int:
+        queue = deque([root])
+        res = []
+
+        while (len(queue) != 0):
+            size = len(queue)
+            cur_level = []
+            for i in range(size):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+                cur_level.append(node.val)
+            res.append(cur_level)
+        return res[-1][0]
